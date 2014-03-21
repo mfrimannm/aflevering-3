604724c9526a5ba2fce12fe6240486b88c9ebdbf
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-03-21 01:55:24 +0100

    har lavet den hurtigere MEN dog
    har brugt en sqrt så den ikke behøver at tjekke så langt op dog virker
    denne ikke for long kun double.
    FIND LØSNING
    og har givet den en timer

--- src/Opgave1.java
+++ src/Opgave1.java
@@ -1,5 +1,7 @@
 // denne kunne hvis den skulle være bedre have en liste over alle primtal
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Opgave1 {
@@ -14,8 +16,14 @@ public class Opgave1 {
 				System.out.println("Program afsluttet");
 				break;
 			}
+			long timebefore = System.currentTimeMillis();
 			String primeFactors = PrimeFactors(num);
+			long timeafter = System.currentTimeMillis();
+			long timeTaken = timeafter - timebefore;
+			double seconds = timeTaken / 1000.00000000000;
 			System.out.println("List of prime factors: " + primeFactors);
+			System.out.println("It took " + seconds
+					+ " sec to do the caluation");
 		}
 		scan.close();
 	}
@@ -29,6 +37,10 @@ public class Opgave1 {
 			return primeFactors;
 		}
 		long i = 2;
+
+		long limitToWhenYouKnowItIsAPrime = (long) Math.sqrt(num);
+		// dette er ikke godt da math.sqrt ikke tager long og derfor laver overflow
+
 		while (num != 1) {
 			if ((num % i) == 0) {
 				primeFactors += i;
@@ -38,7 +50,11 @@ public class Opgave1 {
 				}
 			} else {
 				i++;
+				if (i == limitToWhenYouKnowItIsAPrime) {
+					primeFactors += num;
+					break;
 			}
+		}
 		}
 		return primeFactors;
 	}
