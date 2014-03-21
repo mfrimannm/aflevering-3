ea87cfb26bab1ca1d14a85bcefaf4eded3556c43
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-03-21 01:53:26 +0100

    test

--- src/Test.java
+++ src/Test.java
@@ -7,24 +7,46 @@ public class Test {
 		int num = 0;
 		num = getNumberBetwen(scan, 0, 9);
 		System.out.println(num);
+
 	}
-	
-		private static int getNumberBetwen(Scanner scan, int controlLow,
-				int controlHigh) {
-			int num = 5;
-			System.out.println(controlHigh);
-			while (num < controlHigh && num > controlLow) {
-//				while (!scan.hasNextInt()) {
-//					scan.nextLine();
-//					System.out.println("Du skal intaste et heltal mellem 1 og 9");
-//				}
-				if (!scan.hasNextInt()) {
-					scan.nextLine();
-					System.out.println("Du skal intaste et heltal mellem 1 og 9");
-				}
-			}num = scan.nextInt();
 
-			return num;
+	// private static int getNumberBetwen(Scanner scan, int controlLow,
+	// int controlHigh) {
+	// int num = 5;
+	// System.out.println(controlHigh);
+	// while (num <= controlHigh && num >= controlLow) {
+	// while (!scan.hasNextInt()) {
+	// scan.nextLine();
+	// System.out.println("Du skal intaste et heltal mellem 1 og 9");
+	// }
+	// idoi= scan.nextInt();
+	// if(idoi>)
+	// if (!scan.hasNextInt()) {
+	// scan.nextLine();
+	// System.out.println("Du skal intaste et heltal mellem 1 og 9");
+	// }
+	// num = scan.nextInt();
+	// }
+	// return num;
+	// }
+
+	public static int getNumberBetwen(Scanner scan, int controlLow,	int controlHigh) {
+		int num = 0;
+
+		System.out.println("Indtaste et heltal mellem 1 og 9");
+		while (true) {
+			while (!scan.hasNextInt()) {
+				scan.nextLine();
+				System.out.println("Du skal intaste et heltal mellem 1 og 9");
+			}
+			num = scan.nextInt();
+
+			if (num >= controlLow && num <= controlHigh) {
+				break;
+			} else {
+				System.out.println("Du skal intaste et heltal mellem 1 og 9");
+			}
 		}
-
+		return num;
+	}
 }
